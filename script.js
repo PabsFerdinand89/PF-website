aa999f5084f15751afb3f899e2bfa29d4be9c329
PabsFerdinand89 <pabloantoniohernandez@live.com>
2023-08-08 23:08:16 -0500

    added and edited API section

--- script.js
+++ script.js
@@ -23,16 +23,19 @@ function showSlides() {
 //full page tabs
 function openPage(pageName,elmnt,color) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = $(".tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablink");
+  tablinks = $(".tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
-  document.getElementById(pageName).style.display = "block";
+  $("#pageName").style.display = "block";
   elmnt.style.backgroundColor = color;
 }
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+$("#defaultOpen").click();
+
+
+//Bible verse API
